e8d2f6900f500fbed8b1f00834205ed65b09625b
roskey <lsjsg@outlook.com>
2021-01-24 16:14:32 +0800

    update activation function

--- models/components/common.py
+++ models/components/common.py
@@ -57,7 +57,7 @@ def concatenate_together(x_list, axis=1):
 
 
 def getOutput(x, target_shape=1):
-    x = LeakyReLU()(Dense(64)(x))
+    x = Dense(64, activation=tanh)(x)
     # output prediction
     pred = Dense(target_shape)(x)
     return pred
